1cb9d0c36871e04971387f611d1ac2001861bd7c
NenadRis <plosiguant@gmail.com>
2019-04-04 22:41:37 -0700

    Full Implementation

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # StreamerEventViewer 
+
+The full spec for this project can be seen here: https://gist.github.com/osamakhn/14a378f3107d49de47e0b617a3d5fdf5
+
+The current solution only displays follow events for the streamer. Other events can be added easily using the same subscription pattern that is already in use. Due to time constraints, I have chosen to limit what is added in this version. 
+
+### Deploying to AWS ###
+As a go program, this can be easily deployed to the Elastic BeanStalk, by following the method in the article: https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/go-environment.html
+
+### Potential Improvements ###
+The current code was meant more as a proof of concept, and is not a full production-ready setup. In order to get it production ready, the configuration will have to be externalised, either to a configuration server, or even just an external file, in order to make changes easier, and in order to ensure better security
+
+### Scaling ###
+The current solution should support several hundred requests as it is currently implemented. In order to scale it up, I would break it down into sepearte services, which will allow us to scael each microservice independently of each other. The event listeners will have a separate microservice, running on different instances. In front of the main webapp, we will need to place a load balancer, in order to ensure that the load is optimally distributed. In order to enable and support the scaling, we will need to add metrics, dashboards and alarms to ensure visibility into the performance of the system. 
+
+Since the amount of subscriptions that a single API account can have, I would recommend creating multiple accounts, and using them on a round robin basis, so that the users are always able to establish connections. 
+
+
--- main.go
+++ main.go
@@ -1,32 +1,46 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	twitchApi "github.com/Onestay/go-new-twitch"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/sessions"
+	"github.com/gorilla/websocket"
 	"github.com/markbates/goth"
 	"github.com/markbates/goth/gothic"
 	"github.com/markbates/goth/providers/twitch"
 )
 
 var client *twitchApi.Client
+var followedCallback string
+var events []FollowEvent
+var url string
+var wsConnection *websocket.Conn
 
 func main() {
+	url = "thin-cobra-88.localtunnel.me"
 	client = twitchApi.NewClient("93uv9e2fs5bp8j65wyzdbqu1h7ulth")
 
 	gothic.Store = sessions.NewCookieStore([]byte("secret"))
+	followedCallback = "https://" + url + "/followed"
 
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*")
-	twitchProvider := twitch.New("93uv9e2fs5bp8j65wyzdbqu1h7ulth", "3uf68z2zwti1tt72ica3vk90behzw2", "http://localhost:8080/callback")
+	twitchProvider := twitch.New("93uv9e2fs5bp8j65wyzdbqu1h7ulth", "3uf68z2zwti1tt72ica3vk90behzw2", "https://"+url+"/callback")
 	goth.UseProviders(twitchProvider)
 
 	r.GET("/", renderIndex)
 	r.POST("/login", login)
 	r.GET("/callback", callback)
+	r.POST("/followed", followedCallbackFn)
+	r.GET("/followed", confirmFollowedListener)
+	r.GET("/websocket", wshandler)
 
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
@@ -56,15 +70,95 @@ func callback(c *gin.Context) {
 		c.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
-	/*res, err := json.Marshal(user)
-	if err != nil {
-		c.AbortWithError(http.StatusInternalServerError, err)
-		return
-	}
-	jsonString := string(res)*/
 
 	session, _ := gothic.Store.Get(c.Request, "current_session")
 	streamer := session.Values["streamer"]
 
-	c.HTML(http.StatusOK, "streamer.tmpl", gin.H{"streamer": streamer})
+	streamerData, err := client.GetUsersByLogin(streamer.(string))
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+	id := streamerData[0].ID
+	subscriptionUrl := fmt.Sprintf("https://api.twitch.tv/helix/users/follows?first=1&to_id=%v", id)
+
+	subscribe(c, followedCallback, subscriptionUrl)
+
+	subscriptionUrl = fmt.Sprintf("https://api.twitch.tv/helix/users/follows?first=1&from_id=%v", id)
+
+	subscribe(c, followedCallback, subscriptionUrl)
+
+	c.HTML(http.StatusOK, "streamer.tmpl", gin.H{"streamer": streamer, "url": url})
+}
+
+func subscribe(c *gin.Context, callback string, subscriptionUrl string) {
+	subscriptionRequest := SubscriptionRequest{callback, "subscribe", subscriptionUrl, 864000}
+
+	requestJson, err := json.Marshal(subscriptionRequest)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	req, err := http.NewRequest("POST", "https://api.twitch.tv/helix/webhooks/hub", bytes.NewBuffer(requestJson))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Add("Client-ID", "93uv9e2fs5bp8j65wyzdbqu1h7ulth")
+
+	client := &http.Client{}
+	_, err = client.Do(req)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+}
+
+func confirmFollowedListener(c *gin.Context) {
+	challenge := c.Query("hub.challenge")
+	c.String(http.StatusOK, challenge)
+}
+
+func followedCallbackFn(c *gin.Context) {
+	body, _ := ioutil.ReadAll(c.Request.Body)
+	fmt.Println("request Body:", string(body))
+	var followedEvents FollowedEvents
+	json.Unmarshal(body, &followedEvents)
+
+	newEvents := followedEvents.Data
+	for _, event := range newEvents {
+		wsConnection.WriteMessage(websocket.TextMessage, []byte(event.FromName+" followed "+event.ToName))
+	}
+	fmt.Println(events)
+}
+
+func wshandler(c *gin.Context) {
+	var err error
+	wsConnection, err = wsupgrader.Upgrade(c.Writer, c.Request, nil)
+	fmt.Println("Creating connection")
+	if err != nil {
+		panic(err)
+	}
+}
+
+var wsupgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+type FollowedEvents struct {
+	Data []FollowEvent `json:"data"`
+}
+
+type FollowEvent struct {
+	FromId     string `json:"from_id"`
+	FromName   string `json:"from_name"`
+	ToId       string `json:"to_id"`
+	ToName     string `json:"to_name"`
+	FollowedAt string `json:"followed_at"`
+}
+type SubscriptionRequest struct {
+	Callback     string `json:"hub.callback"`
+	Mode         string `json:"hub.mode"`
+	Topic        string `json:"hub.topic"`
+	LeaseSeconds int    `json:"hub.lease_seconds"`
 }
